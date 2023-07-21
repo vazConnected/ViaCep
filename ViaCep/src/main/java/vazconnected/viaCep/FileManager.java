e8b35273f99862d94377e2bf792d672c6bdee4ba
Pedro Vaz <vaz.connected@gmail.com>
2023-07-21 00:38:26 -0300

    Read local files

--- ViaCep/src/main/java/vazconnected/viaCep/FileManager.java
+++ ViaCep/src/main/java/vazconnected/viaCep/FileManager.java
@@ -1,23 +1,21 @@
 package vazconnected.viaCep;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public final class FileManager {
+    private FileManager() {}
+
     public static void writeFile(String path, String content) throws IOException {
         FileWriter fileWriter = new FileWriter(path);
         fileWriter.write(content);
         fileWriter.close();
     }
 
-    public static void appendToFile(String path, String content) throws IOException {
-        FileWriter fileWriter = new FileWriter(path);
-        fileWriter.append(content);
-        fileWriter.close();
-    }
-
-    public static String readFile(String path) throws FileNotFoundException {
-        BufferedReader bufferedReader = new BufferedReader( new FileReader(path) );
-
+    public static String readFile(File file) throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader( new FileReader(file) );
         String fileContent = "";
 
         String line = null;
@@ -35,4 +33,27 @@ public final class FileManager {
 
         return fileContent;
     }
+
+    public static List<File> getListOfFiles(String directory, Pattern namePattern) {
+        List<File> files = new ArrayList<File>();
+
+        File folder = new File(directory);
+        File[] listOfFiles = folder.listFiles();
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                if (namePattern != null) {
+                    boolean isACepFile = namePattern.matcher(file.getName()).matches();
+
+                    if (isACepFile) {
+                        files.add(file);
+                    }
+                } else {
+                    files.add(file);
+                }
+            }
+        }
+
+        return files;
+    }
+
 }
--- ViaCep/src/main/java/vazconnected/viaCep/Main.java
+++ ViaCep/src/main/java/vazconnected/viaCep/Main.java
@@ -3,10 +3,14 @@ package vazconnected.viaCep;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Main {
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public static void main(String[] args) {
         System.out.println("API ViaCep (https://viacep.com.br/)");
@@ -26,7 +30,7 @@ public class Main {
                     Main.search();
                     break;
                 case(2): // Consultar resultados armazenados
-                    Main.listResults();
+                    Main.consultLocalResults();
                     break;
                 case(0): // Sair
                     System.out.println("Saindo...");
@@ -63,9 +67,9 @@ public class Main {
 
         System.out.println("Resultados obtidos para o cep " + queryResult.cep() + ":\n" + queryResult);
         try {
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            FileManager.writeFile(cep + ".json", gson.toJson(queryResult));
-            System.out.println("O arquivo " + cep + ".json" + " foi criado");
+            String filename = cep + ".json";
+            FileManager.writeFile(filename, Main.gson.toJson(queryResult));
+            System.out.println("O arquivo " + filename + " foi criado");
         } catch (IOException e) {
             System.err.println("Não foi possível armazenar o resultado em um arquivo. Erro:\n" +
                     e.getMessage());
@@ -73,7 +77,47 @@ public class Main {
 
     }
 
-    private static void listResults() {
-        // Todo: ler diretorio de resultados, listar opções e informar resultados do cep selecionado
+    private static void consultLocalResults() {
+        Pattern fileNamePattern = Pattern.compile("^[0-9]{8}.json$");
+
+        ArrayList<File> files = new ArrayList( FileManager.getListOfFiles(".", fileNamePattern) );
+        HashMap<String, File> mapNameWithFile = new HashMap<String, File>();
+
+        if (files.isEmpty()) {
+            System.out.println("Nenhum resultado armazenado foi encontrado.");
+            return;
+        }
+
+        for (File file: files) {
+            mapNameWithFile.put(file.getName(), file);
+        }
+        ArrayList<String> fileNames = new ArrayList<>(mapNameWithFile.keySet().stream().toList());
+        Collections.sort(fileNames);
+
+        System.out.println("Foram encontrados registros locais para os seguintes CEPs:");
+        for (String fileName: fileNames) {
+            System.out.println("\t" + fileName);
+        }
+
+        System.out.println("\nDigite o nome do arquivo que você deseja consultar: ");
+        Scanner scanner = new Scanner(System.in);
+        String selectedFileName = "";
+        do {
+            System.out.print("\nArquivo: ");
+            selectedFileName = scanner.nextLine();
+
+            if (!fileNames.contains(selectedFileName)) {
+                System.err.println("O arquivo selecionado não existe. Tente novamente.");
+            }
+        } while (!fileNames.contains(selectedFileName));
+
+        File selectedFile = mapNameWithFile.get(selectedFileName);
+        try {
+            Address cepInformation = Main.gson.fromJson(FileManager.readFile(selectedFile), Address.class);
+            System.out.println("A seguir as informações obtidas: \n" + cepInformation);
+        } catch (FileNotFoundException e) {
+            System.err.println("Erro ao ler o arquivo de cep: " + e.getMessage());
+        }
     }
+
 }
